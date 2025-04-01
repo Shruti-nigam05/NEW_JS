b7e6c61a817626c8c9fe7aede619d76df3746518
shruti <shrutinigam59@gmail.com>
2025-04-02 03:27:31 +0530

    JS STARTING

--- first.js
+++ first.js
@@ -1,1 +1,11 @@
+// 1. display in document:-
+alert("HEY ITS ME, Shruti");
+//document.write("HEY THIS IS DOCUMENT WRITTEN")
+
+// 2. DISPLAY IN CONSOLE:- 
+console.log("HELLO WORLD!! ");
 console.log("first js code");
+console.warn("this is warning!!! ")
+console.error("this is an ERROR")
+
+// VARIABLES :- Containers to store datatypes.
