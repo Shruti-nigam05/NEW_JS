7eb9331c562e02c1283e9864137a0a4678c41de4
shruti <shrutinigam59@gmail.com>
2025-04-02 04:12:24 +0530

    functions in js

--- first.js
+++ first.js
@@ -1,11 +1,47 @@
-// 1. display in document:-
-alert("HEY ITS ME, Shruti");
-//document.write("HEY THIS IS DOCUMENT WRITTEN")
+// // 1. display in document:-
+// alert("HEY ITS ME, Shruti");
+// //document.write("HEY THIS IS DOCUMENT WRITTEN")
 
-// 2. DISPLAY IN CONSOLE:- 
-console.log("HELLO WORLD!! ");
-console.log("first js code");
-console.warn("this is warning!!! ")
-console.error("this is an ERROR")
+// // 2. DISPLAY IN CONSOLE:- 
+// console.log("HELLO WORLD!! ");
+// console.log("first js code");
+// console.warn("this is warning!!! ")
+// console.error("this is an ERROR")
 
-// VARIABLES :- Containers to store datatypes.
+// // VARIABLES :- Containers to store datatypes.
+
+var num1 = 2;
+var num2 = 5;
+console.log(num1 + num2);
+
+//DATATYPES IN JS 
+
+var num1 = 56;
+var str1 = "this is shruti";
+var marks = {
+    shruti : 52,
+    parul : 55,
+    vidhi : 89
+};
+
+console.log(num1);
+console.log(str1);
+console.log(marks);
+
+let arr = [1,2,3,4,5];
+
+let c = 25;
+c = 55;
+
+c -= 5; // c-= ----> c = c-5
+
+function avg(a , b){
+    return (a + b)/2
+}
+
+let d = avg(14 , 54);
+let d2 = avg(4 , 8)
+console.log(d);
+console.log(d2);
+
+
